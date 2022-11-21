ec956d3aad6e5921099b5b849d4ec1a4366ba2dc
Ilya <sannikov444@yandex.ru>
2022-11-21 10:46:32 +0300

    добавили 2 задачу

---
+++ example002/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b=int.Parse(Console.ReadLine());
+if (a>b){
+    Console.Write("Максимально число из двух ");
+    Console.Write(a);
+}
+else {
+    Console.Write("Максимально число из двух ");
+    Console.Write(b);
+}
