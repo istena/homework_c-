958247f5561e1c102c645c2f4113457cc517795d
Ilya <sannikov444@yandex.ru>
2022-11-21 11:04:43 +0300

    добавили проверку на четность

---
+++ example006/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int a=int.Parse(Console.ReadLine());
+if (a%2==0){
+    Console.Write("Число четное");
+}
+else Console.Write("Число нечетное");
+
