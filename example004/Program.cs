34f4fa405f69b67ef23dbe8960b94459b5b43400
Ilya <sannikov444@yandex.ru>
2022-11-21 10:54:56 +0300

    добавили максимум для 3 чисел

---
+++ example004/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите первое число");
+int a=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b=int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int d=int.Parse(Console.ReadLine());
+int max=a;
+if (b>max) 
+max=b;
+if(d>max)
+max=d;
+Console.Write("Максимально число ");
+Console.Write(max);
